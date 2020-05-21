372df562beba40c1ac469a96429279c8930baffd
Alan Valdez <65427262+AlanEzequielValdez@users.noreply.github.com>
2020-05-21 17:15:18 -0300

    Add files via upload

---
+++ Parte Alan/CajaDeAhorroPesos.java
@@ -0,0 +1,14 @@
+public class CajaDeAhorroPesos extends Cuenta {
+
+	public CajaDeAhorroPesos(String alias, double saldo) {
+		super(alias, saldo);
+	}
+
+	@Override
+	public void quitarSaldo(double saldo) {
+		if(saldo > this.saldo){
+			throw new RuntimeException("No posee el saldo necesario para realizar esta operación.");
+		}
+		this.saldo -= saldo;
+	}
+}
