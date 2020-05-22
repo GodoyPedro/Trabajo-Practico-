5d5550f4d0836d942c8ffd415c29aafecc25bf6c
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-05-22 19:06:50 -0300

    Delete ComprarDolares.java

--- Clases Alan + Operaciones/ComprarDolares.java
+++
@@ -1,9 +0,0 @@
-public class ComprarDolares extends Operacion{
-
-	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, double saldo){
-		cuenta1.quitarSaldo((((saldo)*100)*30)/100);
-		cuenta2.agregarSaldo(saldo);
-		cuenta1.agregarMovimiento("compraDolares", saldo, cuenta1.obtenerSaldo());
-		cuenta2.agregarMovimiento("compraDolares", saldo, cuenta2.obtenerSaldo());
-	}
-}
