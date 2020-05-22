413d390ee575f2c6f9dc391d3ccd207cffa2b839
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-05-22 19:07:37 -0300

    Delete Deposito.java

--- Clases Alan + Operaciones/Deposito.java
+++
@@ -1,7 +0,0 @@
-public class Deposito extends Operacion{
-
-	public void depositar(Cuenta cuenta, double saldo){
-		cuenta.agregarSaldo(saldo);
-		cuenta.agregarMovimiento("deposito", saldo, cuenta.obtenerSaldo());
-	}
-}
