ad93ad0e131935cb14881c4ac6173dee91230da6
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-05-22 18:53:08 -0300

    Update Deposito.java

--- Deposito.java
+++ Deposito.java
@@ -2,10 +2,9 @@ package predeterminado;
 
 public class Deposito extends Operacion {
 	
-	public void depositar(Cuenta cuenta, double monto) {
+	public void depositar(Cuenta cuenta, double monto) throws ErrorAlIntroducirSaldo {
 		
 		cuenta.agregarSaldo(monto);
-
 		
 	}
 }
