ee298ef8f2c0ce02721b238d0f10233d77efd9d8
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-05-22 19:07:43 -0300

    Delete Extraccion.java

--- Clases Alan + Operaciones/Extraccion.java
+++
@@ -1,7 +0,0 @@
-public class Extraccion extends Operacion{
-
-	public void extraerFondos(Cuenta cuenta, double saldo){
-		cuenta.quitarSaldo(saldo);
-		cuenta.agregarMovimiento("retirar", saldo, cuenta.obtenerSaldo());
-	}
-}
