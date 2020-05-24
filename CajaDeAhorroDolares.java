c060c4c653ac958255609f47ac1e14a2ceebdf9f
Alan Valdez <65427262+AlanEzequielValdez@users.noreply.github.com>
2020-05-24 20:05:23 -0300

    Update CajaDeAhorroDolares.java

--- CajaDeAhorroDolares.java
+++ CajaDeAhorroDolares.java
@@ -5,7 +5,7 @@ public class CajaDeAhorroDolares extends Cuenta{
 	}
 
 	@Override
-	public void quitarSaldo(double saldo) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
+	public void quitarSaldo(int saldo) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
 		if(saldo > this.saldo){
 			throw new ErrorSaldoInsuficiente();
 		}else if(saldo < 0){
