f18bf642b5398121475933983be05d42af7bc6b6
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-05-24 20:00:13 -0300

    Update ComprarDolares.java

--- ComprarDolares.java
+++ ComprarDolares.java
@@ -1,12 +1,10 @@
-package predeterminado;
-
 public class ComprarDolares extends Operacion {
 	
-	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, double montoEnDolares) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo { 
+	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, int montoEnDolares) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo { 
 		
 		double montoEnPesos = montoEnDolares / 0.015;  //
 		cuenta1.quitarSaldo(montoEnPesos * 1.3); //<--- Impuesto
-		cuenta2.agregarSaldo( montoEnDolares);
+		cuenta2.agregarSaldo(montoEnDolares);
 		
 	}
 }
