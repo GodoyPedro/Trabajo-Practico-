8d0af64705f47e5f1396d3633f54ea22d46ae73a
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-05-24 20:25:24 -0300

    Update CuentaCorriente.java

--- CuentaCorriente.java
+++ CuentaCorriente.java
@@ -4,17 +4,23 @@ public class CuentaCorriente extends Cuenta {
 	private double descubierto;
 	
 	public CuentaCorriente(String alias, double saldo, double descubierto) throws ErrorAlIntroducirSaldo {
+		
 		super(alias, saldo, descubierto);
 		this.descubierto = descubierto;
 	}
 
 	@Override
 	public void quitarSaldo(double saldo) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
+		
 		if((-1)*descubierto > this.saldo - saldo){
+			
 			throw new ErrorSaldoInsuficiente();
-		}else if(saldo < 0){
+		}
+		else if(saldo < 0){
+			
 			throw new ErrorAlIntroducirSaldo();
 		}
+		
 		this.saldoPrevio = this.saldo;
 		this.saldo -= saldo;
 	}
