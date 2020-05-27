99ab867a7f06278ce61bb456442e773e4aa0848c
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-05-27 18:20:46 -0300

    Update Transferencia.java

--- Transferencia.java
+++ Transferencia.java
@@ -3,7 +3,7 @@ public class Transferencia extends Operacion implements Reversible {
 	
 	public void transferir(Cuenta cuenta1, Cuenta cuenta2, int montoATransferir) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo, ErrorTransferencia {
 		
-		verificarTransferencia(cuenta1, cuenta2);
+//		verificarTransferencia(cuenta1, cuenta2);
 		cuenta1.quitarSaldo(montoATransferir);
 		cuenta2.agregarSaldo(montoATransferir);
 
@@ -18,12 +18,12 @@ public class Transferencia extends Operacion implements Reversible {
 
 	}
 	
-	private void verificarTransferencia(Cuenta cuenta1, Cuenta cuenta2) throws ErrorTransferencia {
-		
-		if(cuenta1.getClass().getName().equals("CajaDeAhorroDolares") || 
-				cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
-			
-			throw new ErrorTransferencia("No se puede transferir usando una cuenta en dolares");
-		}
-	}
+//	private void verificarTransferencia(Cuenta cuenta1, Cuenta cuenta2) throws ErrorTransferencia {
+//		
+//		if(cuenta1.getClass().getName().equals("CajaDeAhorroDolares") || 
+//				cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
+//			
+//			throw new ErrorTransferencia("No se puede transferir usando una cuenta en dolares");
+//		}
+//	}
 }
