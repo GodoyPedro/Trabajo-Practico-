8e81c444711bb5d0f6061bbdfd5b6af12925fa92
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-05-27 19:36:19 -0300

    Update Extraccion.java

--- Extraccion.java
+++ Extraccion.java
@@ -1,11 +1,8 @@
-
-
 public class Extraccion extends Operacion {
 	
-	public void extraerFondos(Cuenta cuenta, int monto) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo, ErrorCuentaInvalida { 
+	public void extraerFondos(Cuenta cuenta, int monto) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo{ 
 		
 		cuenta.quitarSaldo(monto);
 		
 	}
 }
-
