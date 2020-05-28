00b29fcb5d08319726b3475efd8fe56f56d00767
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-05-28 20:29:51 -0300

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -2,11 +2,25 @@ import java.util.Scanner;
 
 public class ATM {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
+		boolean numeroEncontrado = false;
+		String[] datos = null;
+		Scanner numeroTarjeta = new Scanner(System.in);;
+	
+		BaseDeDatos base = new BaseDeDatos();
 		System.out.println("INGRESE EL NUMERO DE LA TARJETA");
-		Scanner numeroTarjeta = new Scanner(System.in);
-		Tarjeta tarjeta = new Tarjeta(numeroTarjeta.nextLine());
+		
+		while(!numeroEncontrado) {
+			
+			String numeroUsuario = numeroTarjeta.nextLine();
+			
+			datos = base.buscarNumeroTarjeta(numeroUsuario);
+			
+			numeroEncontrado = !(datos == null);
+		}
+		
+		Tarjeta tarjeta = new Tarjeta(datos[1],datos[2]);
 		System.out.println("INGRESE EL PIN DE LA TARJETA");
 		tarjeta.validarPinTarjeta(tarjeta);
 		Cajero cajero = new Cajero(tarjeta);
