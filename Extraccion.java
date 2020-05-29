c42d766b583683f74299965993427d6ac9b5d2c6
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-05-29 20:54:34 -0300

    Update Extraccion.java

--- Extraccion.java
+++ Extraccion.java
@@ -2,7 +2,13 @@ public class Extraccion extends Operacion {
 	
 	public void extraerFondos(Cuenta cuenta, int monto) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo{ 
 		
-		cuenta.quitarSaldo(monto);
+		if(monto%100 != 0) {
+			
+			System.err.print("No se puede seleccionar montos que no sean multiplos de 100 prueba");	
+		}
+		
+		
+		cuenta.retirarSaldo(monto);
 		
 	}
 }
