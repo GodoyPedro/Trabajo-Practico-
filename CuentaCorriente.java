4dbf6fabd9234771c76c7a0091a563f6aed4fb40
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-05-29 20:55:37 -0300

    Update CuentaCorriente.java

--- CuentaCorriente.java
+++ CuentaCorriente.java
@@ -1,26 +1,25 @@
 public class CuentaCorriente extends Cuenta {
-	
-	//El descubierto tiene que ser si o si positivo
+
+	// El descubierto tiene que ser si o si positivo
 	private double descubierto;
-	
+
 	public CuentaCorriente(String alias, double saldo, double descubierto) throws ErrorAlIntroducirSaldo {
-		
+
 		super(alias, saldo, descubierto);
 		this.descubierto = descubierto;
 	}
 
 	@Override
-	public void quitarSaldo(double saldo) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
-		
-		if((-1)*descubierto > this.saldo - saldo){
-			
+	public void retirarSaldo(double saldo) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
+
+		if ((-1) * descubierto > this.saldo - saldo) {
+
 			throw new ErrorSaldoInsuficiente();
-		}
-		else if(saldo < 0){
-			
+		} else if (saldo < 0) {
+
 			throw new ErrorAlIntroducirSaldo();
 		}
-		
+
 		this.saldoPrevio = this.saldo;
 		this.saldo -= saldo;
 	}
