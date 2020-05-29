3911dd1b288b263f437a9102f1e3d847d3c15bb0
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-05-29 20:49:37 -0300

    Update Transferencia.java

--- Transferencia.java
+++ Transferencia.java
@@ -1,18 +1,31 @@
 public class Transferencia extends Operacion implements Reversible {
-	
-	
-	public void transferir(Cuenta cuenta1, Cuenta cuenta2, int montoATransferir) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
+
+	public void transferir(Cuenta cuenta1, Cuenta cuenta2, int montoATransferir)
+			throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
+
+		if (cuenta1.obtenerAlias().equals(cuenta2.obtenerAlias())) {
+
+			System.err.print("NO PODES TRANSFERIR A LA MISMA CUENTA");
+		} else if (cuenta1.getClass().getName().equals("CajaDeAhorroDolares")
+				|| cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
+			
+			System.err.print("NO SE PUEDE OPERAR CON UNA CUENTA EN DOLARES");
+		}
+		else {
+			cuenta1.retirarSaldo(montoATransferir);
+			cuenta2.depositarSaldo(montoATransferir);
+			
+		}
 		
-		cuenta1.quitarSaldo(montoATransferir);
-		cuenta2.agregarSaldo(montoATransferir);
 
 	}
-	public void revertirMovimiento(Cuenta...cuentas) {
 
-		for(Cuenta cuenta : cuentas) {
-			
+	public void revertirMovimiento(Cuenta... cuentas) {
+
+		for (Cuenta cuenta : cuentas) {
+
 			cuenta.cambiarSaldo(cuenta.obtenerSaldoPrevio());
-			
+
 		}
 
 	}
