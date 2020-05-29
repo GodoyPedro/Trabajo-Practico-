6aa64b3b7c37cc32abbd8631919671cb21633d78
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-05-29 20:55:01 -0300

    Update Deposito.java

--- Deposito.java
+++ Deposito.java
@@ -2,7 +2,7 @@ public class Deposito extends Operacion {
 	
 	public void depositar(Cuenta cuenta, int monto) throws ErrorAlIntroducirSaldo {
 		
-		cuenta.agregarSaldo(monto);
+		cuenta.depositarSaldo(monto);
 		
 	}
 }
