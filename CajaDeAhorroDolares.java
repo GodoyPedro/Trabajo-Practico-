fa107586cb7a4667634db0c26a2554a69ca5ce73
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-05-29 21:08:38 -0300

    Update CajaDeAhorroDolares.java

--- CajaDeAhorroDolares.java
+++ CajaDeAhorroDolares.java
@@ -1,20 +1,21 @@
-public class CajaDeAhorroDolares extends Cuenta{
+
+public class CajaDeAhorroDolares extends Cuenta {
 
 	public CajaDeAhorroDolares(String alias, double saldo) throws ErrorAlIntroducirSaldo {
 		super(alias, saldo);
 	}
 
 	@Override
-	public void quitarSaldo(double saldo) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
-		
-		if(saldo > this.saldo) {
-			
+	public void retirarSaldo(double saldo) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
+
+		if (saldo > this.saldo) {
+
 			throw new ErrorSaldoInsuficiente();
-			
-		}else if(saldo < 0) {
-			
+
+		} else if (saldo < 0) {
+
 			throw new ErrorAlIntroducirSaldo();
-			
+
 		}
 		this.saldoPrevio = this.saldo;
 		this.saldo -= saldo;
