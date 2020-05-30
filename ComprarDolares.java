49cd57f7bee4040a414297b7cba362c88e92e08a
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-05-29 21:07:08 -0300

    Update ComprarDolares.java

--- ComprarDolares.java
+++ ComprarDolares.java
@@ -1,15 +1,17 @@
 public class ComprarDolares extends Operacion {
-	
-	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, int montoEnDolares) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo, ErrorCuentaInvalida { 
-		
-//		if(cuenta1.getClass().getName().equals("CajaDeAhorroDolares") || !cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
-//			
-//			throw new ErrorCuentaInvalida("No se puede operar con estas cuentas");
-//		}
-		
-		double montoEnPesos = montoEnDolares / 0.015;  //
-		cuenta1.quitarSaldo(montoEnPesos * 1.3); //<--- Impuesto
-		cuenta2.agregarSaldo(montoEnDolares);
-		
+
+	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, int montoEnDolares)
+			throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo, ErrorCuentaInvalida {
+
+		if (cuenta1.getClass().getName().equals("CajaDeAhorroDolares")
+				|| !cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
+
+			throw new ErrorCuentaInvalida("No se puede operar con estas cuentas");
+		}
+
+		double montoEnPesos = montoEnDolares / 0.015; //
+		cuenta1.retirarSaldo(montoEnPesos * 1.3); // <--- Impuesto
+		cuenta2.depositarSaldo(montoEnDolares);
+
 	}
 }
