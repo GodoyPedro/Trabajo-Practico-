7d9be47c11eaa9ab20a7641b5fab74c003676141
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-05-29 21:09:06 -0300

    Update BaseDeDatos.java

--- BaseDeDatos.java
+++ BaseDeDatos.java
@@ -1,28 +1,28 @@
 import java.util.List;
 
 public class BaseDeDatos {
-	
+
 	OperadorDeArchivos operador;
-	
+
 	public BaseDeDatos() {
-		
+
 		operador = new OperadorDeArchivos();
 	}
-	
-	public String[] buscarNumeroTarjeta(String numero){
-		
+
+	public String[] buscarNumeroTarjeta(String numero) {
+
 		String[] datos = operador.analizarArchivoValidacion(numero);
-		
-		if(datos != null) {
-			
+
+		if (datos != null) {
+
 			return datos;
 		}
-		
+
 		return null;
 	}
 
 	public void escribirArchivoMovimientos(List<String> movimientos) {
-		
+
 		operador.escribirArchivoMovimientos(movimientos);
 	}
 
@@ -31,18 +31,17 @@ public class BaseDeDatos {
 		operador.escribirArchivoTickets(movimiento);
 	}
 
-	public void escritorArchivoAlias(List<String> listaAlias) {
+	public void escribirArchivoAlias(List<String> listaAlias) {
 
 		operador.escritorArchivoAlias(listaAlias);
-		
+
 	}
 
 	public List<Cuenta> analizarArchivoClientes(String cuit) {
 
 		List<String> listaAlias = operador.analizarArchivoClientes(cuit);
-		
+
 		return operador.analizarArchivoCuentas(listaAlias);
 	}
-	
-	
+
 }
