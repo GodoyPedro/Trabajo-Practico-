b8431d6cd2f8755e2c6327540ef17126aa74dc2b
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-05-31 18:54:19 -0300

    Update Transferencia.java

--- Trabajo terminado/Transferencia.java
+++ Trabajo terminado/Transferencia.java
@@ -1,13 +1,15 @@
 public class Transferencia extends Operacion implements Reversible {
 	
-	
+	//Transfiere un monto desde una cuenta a otra. No se pueden realizar transferencias que involucren una cuenta en dólares.
 	public void transferir(Cuenta cuenta1, Cuenta cuenta2, int montoATransferir) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
 		
+		//El usuario no puede realizar una transferencia ingresando el mismo alias para ambas cuentas.
 		if(cuenta1.obtenerAlias().equals(cuenta2.obtenerAlias())){
 			
 			System.err.println("NO PODES TRANSFERIR A LA MISMA CUENTA");
 		}
 		
+		//Si el usuario ingresa un alias que pertenece a una caja de ahorro en dólares, se imprime un mensaje de error.
 		else if(cuenta1.getClass().getName().equals("CajaDeAhorroDolares") || 
 				cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
 			
@@ -18,6 +20,8 @@ public class Transferencia extends Operacion implements Reversible {
 		cuenta2.depositarSaldo(montoATransferir);
 
 	}
+	//Este método permite revertir la última transferencia realizada. Cuando se lo llama, las dos cuentas involucradas en la transferencia vuelven a su estado previo a realizar el
+	//movimiento.
 	public void revertirMovimiento(Cuenta...cuentas) {
 
 		for(Cuenta cuenta : cuentas) {
