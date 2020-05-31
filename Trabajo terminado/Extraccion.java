19201d439fb4984ba72eb222f2e2fe8e6c465765
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-05-31 18:27:10 -0300

    Update Extraccion.java

--- Trabajo terminado/Extraccion.java
+++ Trabajo terminado/Extraccion.java
@@ -2,8 +2,11 @@
 
 public class Extraccion extends Operacion {
 	
+	
+	//Extrae el monto ingresado desde la cuenta indicada.
 	public void extraerFondos(Cuenta cuenta, int monto) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo{ 
 		
+		//Si el monto no es un múltiplo de 100, imprime el mensaje de error. Esto se debe a que el cajero solo dispensa billetes de 1000, de 500 y de 100.
 		if(monto % 100 != 0) {
 			
 			System.err.println("No se puede seleccionar montos que no sean multiplos de 100");	
@@ -13,4 +16,3 @@ public class Extraccion extends Operacion {
 		
 	}
 }
-
