58637f788db422c2eb21ac4389e79b913fee2c41
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-05-31 18:53:26 -0300

    Update Deposito.java

--- Trabajo terminado/Deposito.java
+++ Trabajo terminado/Deposito.java
@@ -1,7 +1,7 @@
 
-
 public class Deposito extends Operacion {
 	
+	//Deposita el monto ingresado en la cuenta indicada.
 	public void depositar(Cuenta cuenta, int monto) throws ErrorAlIntroducirSaldo {
 		
 		cuenta.depositarSaldo(monto);
