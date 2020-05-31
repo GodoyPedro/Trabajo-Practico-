3e4f0c9cd372bd0e0ff8023b7bbec4e304be0b7c
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-05-31 18:53:09 -0300

    Update ConsultarSaldo.java

--- Trabajo terminado/ConsultarSaldo.java
+++ Trabajo terminado/ConsultarSaldo.java
@@ -1,11 +1,10 @@
 
 public class ConsultarSaldo extends Operacion {
 	
+	//Devuelve el saldo de la cuenta indicada.
 	public double consultarSaldo(Cuenta cuenta) {
 		
 		return cuenta.obtenerSaldo();
 		
 	}
 }
-
-
