2d44b748f21b642d974ad54ed4a294d8ce42a007
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-05-31 18:52:41 -0300

    Update ConsultarAlias.java

--- Trabajo terminado/ConsultarAlias.java
+++ Trabajo terminado/ConsultarAlias.java
@@ -2,7 +2,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ConsultarAlias extends Operacion {
-
+	
+	//Devuelve todos los alias de todas las cuentas del usuario.
 	public List<String> obtenerListaAlias(List<Cuenta> listaCuentas) {
 		
 		List<String> listaAlias = new LinkedList<String>();
