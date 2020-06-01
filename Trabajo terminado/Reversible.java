956a330730d410fc68dc9d71caf5b6bb35728c32
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-06-01 15:20:17 -0300

    Update Reversible.java

--- Trabajo terminado/Reversible.java
+++ Trabajo terminado/Reversible.java
@@ -1,7 +1,13 @@
 
 
 public interface Reversible {
-	
-	public void revertirMovimiento(Cuenta...cuentas);
+
+	/**
+	 * pre: Las cuentas ingresadas son las cuales se va revertir el ultimo
+	 * movimiento realizado en caso de ser una Transferencia.
+	 * 
+	 * @param cuentas
+	 */
+	public void revertirMovimiento(Cuenta... cuentas);
 
 }
