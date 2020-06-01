2fa6ed74527f6da82d092ec47fc5d995656f40bc
PabloCamussi <65427236+PabloCamussi@users.noreply.github.com>
2020-06-01 14:47:21 -0300

    Update ComprarDolares.java

--- Trabajo terminado/ComprarDolares.java
+++ Trabajo terminado/ComprarDolares.java
@@ -4,7 +4,8 @@ public class ComprarDolares extends Operacion {
 	//El usuario ingresa el monto en dólares que quiere depositar en su caja de ahorro en dólares.
 	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, int montoEnDolares) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo { 
 		
-		double montoEnPesos = montoEnDolares / 0.015; //Se hace el pasaje de pesos a dólares. 1 dólar = 66.66 pesos.
+		double montoEnPesos = montoEnDolares / 0.015; //Se hace el pasaje de pesos a dólares. 1 dólar = 66.66 pesos (Dato extraído según
+		                                              //Google. Actualizado a fecha de 23/5/2020.
 		cuenta1.retirarSaldo(montoEnPesos * 1.3); //Se aplica el impuesto de 30% a la compra de dólares.
 		cuenta2.depositarSaldo(montoEnDolares);
 		
