581dace028e36457246a2445b808b8704ad43a5b
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-06-01 15:15:39 -0300

    Update HistorialMovimientos.java

--- Trabajo terminado/HistorialMovimientos.java
+++ Trabajo terminado/HistorialMovimientos.java
@@ -1,21 +1,26 @@
 import java.util.List;
 
-public class HistorialMovimientos extends Operacion{
-	
-	//Mediante este método, guardamos las operaciones realizadas sobre una cuenta en una lista. Se pueden guardar hasta 10 movimientos, si se excede este límite,
-	//se elimina el primer movimiento de la lista (es decir, el más viejo) y se agrega uno nuevo.
+public class HistorialMovimientos extends Operacion {
+
+	/**
+	 * pre: Mediante este método, guardamos las operaciones realizadas sobre una
+	 * cuenta en una lista. Se pueden guardar hasta 10 movimientos, si se excede
+	 * este límite, se elimina el primer movimiento de la lista (es decir, el más
+	 * viejo) y se agrega uno nuevo.
+	 * 
+	 * @param lista
+	 * @param movimiento
+	 */
 	public void agregarMovimiento(List<String> lista, String movimiento) {
-		
-		if(lista.size() == 10) {
-			
+
+		if (lista.size() == 10) {
+
 			lista.remove(0);
 			lista.add(movimiento);
-		}
-		else {
-			
+		} else {
+
 			lista.add(movimiento);
 		}
-		
-		
+
 	}
 }
