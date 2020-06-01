612a08938b882d1b1cd541d5b96e1619410c263d
Alan Valdez <65427262+AlanEzequielValdez@users.noreply.github.com>
2020-06-01 16:18:28 -0300

    Update ErrorCuentaInvalida.java

--- Trabajo terminado/ErrorCuentaInvalida.java
+++ Trabajo terminado/ErrorCuentaInvalida.java
@@ -1,5 +1,14 @@
 
+
 public class ErrorCuentaInvalida extends Exception {
+	
+	/**
+	 * Pre: Esta excepcion se utiliza en: ComprarDolares.
+	 * 
+	 * Post: Lanza una excepcion cuando se intenta comprar dolares con una cuenta que no 
+	 *       puede realizar dicha operacion.
+	 * 
+	 */
 	
 	public ErrorCuentaInvalida(String mensaje) {
 		
