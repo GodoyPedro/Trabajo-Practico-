b629a154e140ca9c03781ab3cafc9aac9e6898da
Alan Valdez <65427262+AlanEzequielValdez@users.noreply.github.com>
2020-06-01 16:19:35 -0300

    Update ErrorAlIntroducirSaldo.java

--- Trabajo terminado/ErrorAlIntroducirSaldo.java
+++ Trabajo terminado/ErrorAlIntroducirSaldo.java
@@ -1,14 +1,15 @@
 public class ErrorAlIntroducirSaldo extends Exception {
 
- /**
- * Pre: Esta excepcion se utiliza en: Cuenta, CuentaCorriente, 
- *      CajaDeAhorroDolares, CajaDeAhorroPesos, ComprarDolares, 
- *      Transferencia, Extraccion, Deposito, OperadorDeArchivos, Cajero
- * 
- * Post: Lanza una excepcion cuando en un parametro se utiliza un valor negativo, 
- *       generalmente es lo relacionado al saldo de la cuenta
- * 
- */
+	/**
+	 * Pre: Esta excepcion se utiliza en: Cuenta, CuentaCorriente, 
+	 *      CajaDeAhorroDolares, CajaDeAhorroPesos, ComprarDolares, 
+	 *      Transferencia, Extraccion, Deposito, OperadorDeArchivos, Cajero.
+	 * 
+	 * Post: Lanza una excepcion cuando en un parametro se utiliza un valor negativo, 
+	 *       generalmente es lo relacionado al saldo de la cuenta.
+	 * 
+	 */
+
     public ErrorAlIntroducirSaldo()	{
     	
         super("El saldo a introducir debe ser positivo");
