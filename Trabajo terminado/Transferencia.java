8163cfe9349bd3638b5b7f64b8a7b81082486078
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-06-01 15:30:13 -0300

    Update Transferencia.java

--- Trabajo terminado/Transferencia.java
+++ Trabajo terminado/Transferencia.java
@@ -1,33 +1,51 @@
 public class Transferencia extends Operacion implements Reversible {
-	
-	//Transfiere un monto desde una cuenta a otra. No se pueden realizar transferencias que involucren una cuenta en dólares.
-	public void transferir(Cuenta cuenta1, Cuenta cuenta2, int montoATransferir) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
-		
-		//El usuario no puede realizar una transferencia ingresando el mismo alias para ambas cuentas.
-		if(cuenta1.obtenerAlias().equals(cuenta2.obtenerAlias())){
-			
+
+	/**
+	 * pre: El monto ingresado debe ser mayor a cero. No se pueden realizar
+	 * transferencias que involucren una cuenta en dólares.
+	 * 
+	 * post: Se transfiere el monto ingresado de la cuenta1 a la cuenta2
+	 * 
+	 * @param cuenta1
+	 * @param cuenta2
+	 * @param montoATransferir
+	 * @throws ErrorSaldoInsuficiente
+	 * @throws ErrorAlIntroducirSaldo
+	 */
+	public void transferir(Cuenta cuenta1, Cuenta cuenta2, int montoATransferir)
+			throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo {
+
+		// El usuario no puede realizar una transferencia ingresando el mismo alias para
+		// ambas cuentas.
+		if (cuenta1.obtenerAlias().equals(cuenta2.obtenerAlias())) {
+
 			System.err.println("NO PODES TRANSFERIR A LA MISMA CUENTA");
 		}
-		
-		//Si el usuario ingresa un alias que pertenece a una caja de ahorro en dólares, se imprime un mensaje de error.
-		else if(cuenta1.getClass().getName().equals("CajaDeAhorroDolares") || 
-				cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
-			
+
+		// Si el usuario ingresa un alias que pertenece a una caja de ahorro en dólares,
+		// se imprime un mensaje de error.
+		else if (cuenta1.getClass().getName().equals("CajaDeAhorroDolares")
+				|| cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
+
 			System.err.println("NO SE PUEDE OPERAR CON UNA CUENTA EN DOLARES");
 		}
-		
+
 		cuenta1.retirarSaldo(montoATransferir);
 		cuenta2.depositarSaldo(montoATransferir);
 
 	}
-	//Este método permite revertir la última transferencia realizada. Cuando se lo llama, las dos cuentas involucradas en la transferencia vuelven a su estado previo a realizar el
-	//movimiento.
-	public void revertirMovimiento(Cuenta...cuentas) {
 
-		for(Cuenta cuenta : cuentas) {
-			
+	/**
+	 * post: Permite revertir la última transferencia realizada. Cuando se lo
+	 * llama, las dos cuentas involucradas en la transferencia vuelven a su estado
+	 * previo a realizar el movimiento.
+	 */
+	public void revertirMovimiento(Cuenta... cuentas) {
+
+		for (Cuenta cuenta : cuentas) {
+
 			cuenta.cambiarSaldo(cuenta.obtenerSaldoPrevio());
-			
+
 		}
 
 	}
