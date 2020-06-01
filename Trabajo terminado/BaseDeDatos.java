0d278c4ae56a66aa9f1180051c21185849c9495a
Pedro Godoy <63011537+GodoyPedro@users.noreply.github.com>
2020-06-01 16:12:30 -0300

    Update BaseDeDatos.java

--- Trabajo terminado/BaseDeDatos.java
+++ Trabajo terminado/BaseDeDatos.java
@@ -1,5 +1,8 @@
 import java.util.List;
-
+/**
+ * Es el intermediario entre el cajero y los datos que almacenamos en distintas clases.
+ * Si queremos buscar algun dato que no este en el cajero, usamos esta clase.
+ */
 public class BaseDeDatos {
 	
 	OperadorDeArchivos operador;
@@ -9,40 +12,55 @@ public class BaseDeDatos {
 		operador = new OperadorDeArchivos();
 	}
 	
+	/**
+	 * Chequeo si existe el numero de la tarjeta que ingreso el usuario
+	 * @param numero
+	 * @return
+	 */
 	public String[] buscarNumeroTarjeta(String numero){
 		
 		String[] datos = operador.analizarArchivoValidacion(numero);
 		
-		if(datos != null) {
-			
-			return datos;
-		}
-		
-		return null;
+		return datos != null? datos:null;
+	}
+	
+	
+	/**
+	 * Uso el operador de archivos para crear las cuentas 
+	 * @param cuit
+	 * @return listaDeCuentas
+	 */
+	public List<Cuenta> analizarArchivoClientes(String cuit) {
+
+		return operador.analizarArchivoCuentas(operador.analizarArchivoClientes(cuit));
 	}
 
+	/**
+	 * Uso el operador de archivos para escribri el archivo de movimientos con el formato pedido(fecha,concepto,alias,monto)
+	 * @param movimientos
+	 */
 	public void escribirArchivoMovimientos(List<String> movimientos) {
 		
 		operador.escribirArchivoMovimientos(movimientos);
 	}
-
+	
+	/**
+	 * Uso el operador de archivos para escribri el archivo txt donde voy a guardar todos los tickets que se producen al realizar una operacion
+	 * @param movimiento
+	 */
 	public void escribirArchivoTickets(String movimiento) {
 
 		operador.escribirArchivoTickets(movimiento);
 	}
-
+	
+	/**
+	 * Escribo el archivo txt en donde voy a guardar los alias que posee el usuario
+	 * @param listaAlias
+	 */
 	public void escribirArchivoAlias(List<String> listaAlias) {
 
-		operador.escritorArchivoAlias(listaAlias);
-		
+		operador.escritorArchivoAlias(listaAlias);	
 	}
 
-	public List<Cuenta> analizarArchivoClientes(String cuit) {
-
-		List<String> listaAlias = operador.analizarArchivoClientes(cuit);
-		
-		return operador.analizarArchivoCuentas(listaAlias);
-	}
-	
 	
 }
