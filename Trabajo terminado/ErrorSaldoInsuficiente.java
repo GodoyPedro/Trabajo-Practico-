b05b7c11aaa2d47d218919e9aaae0588f340f612
Alan Valdez <65427262+AlanEzequielValdez@users.noreply.github.com>
2020-06-01 16:23:32 -0300

    Update ErrorSaldoInsuficiente.java

--- Trabajo terminado/ErrorSaldoInsuficiente.java
+++ Trabajo terminado/ErrorSaldoInsuficiente.java
@@ -1,8 +1,17 @@
 public class ErrorSaldoInsuficiente extends Exception {
 
+	/**
+	 * Pre: Esta excepcion se utiliza en: Cuenta, CuentaCorriente, CajaDeAhorroPesos, 
+	 *      CajaDeAhorroDolares, Extraccion, Transferencia, ComprarDolares, Cajero.
+	 * 
+	 * Post: Lanza una excepcion cuando se intenta retirar un monto mas grande 
+	 *       que el saldo actual de la cuenta.
+	 * 
+	 */
+	
     public ErrorSaldoInsuficiente()	{
     	
-        super("Usted no posee el saldo suficiente para realizar esta operación");
+        super("Usted no posee el saldo suficiente para realizar esta operaciÃ³n");
         
     }
 }
