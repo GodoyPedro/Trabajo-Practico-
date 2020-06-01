460975ba710fe255741d14feab8466d0d550cfcf
Alan Valdez <65427262+AlanEzequielValdez@users.noreply.github.com>
2020-06-01 16:18:54 -0300

    Update ErrorDatosInvalidos.java

--- Trabajo terminado/ErrorDatosInvalidos.java
+++ Trabajo terminado/ErrorDatosInvalidos.java
@@ -1,6 +1,12 @@
 
 public class ErrorDatosInvalidos extends Exception {
 	
+	/**
+	 * Pre: Esta excepcion se utiliza en: OperadorDeArchivos.
+	 *
+	 *Post: Lanza una excepcion cuando el numero de tarjeta ingresado no existe en la base de datos.
+	 */
+	
 	public ErrorDatosInvalidos(String mensaje) {
 		
 		super(mensaje);
