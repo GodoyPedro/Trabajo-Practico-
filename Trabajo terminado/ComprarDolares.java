77d0c95ee2111dbbd98bad111a942729bfd25c70
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-06-01 15:58:40 -0300

    Update ComprarDolares.java

--- Trabajo terminado/ComprarDolares.java
+++ Trabajo terminado/ComprarDolares.java
@@ -1,13 +1,32 @@
 public class ComprarDolares extends Operacion {
-	
-	//Extrae dinero en pesos desde una cuenta corriente o caja de ahorro en pesos, y lo deposita como dólares en una caja de ahorro en dólares.
-	//El usuario ingresa el monto en dólares que quiere depositar en su caja de ahorro en dólares.
-	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, int montoEnDolares) throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo { 
-		
-		double montoEnPesos = montoEnDolares / 0.015; //Se hace el pasaje de pesos a dólares. 1 dólar = 66.66 pesos (Dato extraído según
-		                                              //Google. Actualizado a fecha de 23/5/2020.
-		cuenta1.retirarSaldo(montoEnPesos * 1.3); //Se aplica el impuesto de 30% a la compra de dólares.
+
+	/**
+	 * pre: La primer cuenta ingresada no puede ser una Caja De Ahorro en dolares y
+	 * la segunda debe ser una Caja de ahorro Dolares. El monto ingresado debe ser mayor a cero
+	 * post: Extrae dinero en pesos desde una cuenta corriente o caja de ahorro en pesos, y lo deposita como dólares en una caja de ahorro en dólares.
+	 * El usuario ingresa el monto en dólares que quiere depositar en su caja de ahorro en dólares.
+	 * 
+	 * @param cuenta1
+	 * @param cuenta2
+	 * @param montoEnDolares
+	 * @throws ErrorSaldoInsuficiente
+	 * @throws ErrorAlIntroducirSaldo
+	 * @throws ErrorCuentaInvalida
+	 */
+	public void comprarDolares(Cuenta cuenta1, Cuenta cuenta2, int montoEnDolares)
+			throws ErrorSaldoInsuficiente, ErrorAlIntroducirSaldo, ErrorCuentaInvalida {
+
+		if (cuenta1.getClass().getName().equals("CajaDeAhorroDolares")
+				|| !cuenta2.getClass().getName().equals("CajaDeAhorroDolares")) {
+
+			throw new ErrorCuentaInvalida("No se puede operar con estas cuentas");
+		}
+
+		double montoEnPesos = montoEnDolares / 0.015; // Se hace el pasaje de pesos a dólares. 1 dólar = 66.66 pesos
+														// (Dato extraído según
+														// Google. Actualizado a fecha de 23/5/2020.
+		cuenta1.retirarSaldo(montoEnPesos * 1.3); // Se aplica el impuesto de 30% a la compra de dólares.
 		cuenta2.depositarSaldo(montoEnDolares);
-		
+
 	}
 }
