e7e9e5d0ecf9c3138b47d72de2d0cc148d05acd1
Braian <44708422+BraianGazano@users.noreply.github.com>
2020-06-01 15:07:45 -0300

    Update Deposito.java

--- Trabajo terminado/Deposito.java
+++ Trabajo terminado/Deposito.java
@@ -1,7 +1,14 @@
 
 public class Deposito extends Operacion {
 	
-	//Deposita el monto ingresado en la cuenta indicada.
+	
+	/**
+	 * pre: El monto debe ser mayor a cero
+	 * post: Deposita el monto ingresado en la cuenta indicada
+	 * @param cuenta
+	 * @param monto
+	 * @throws ErrorAlIntroducirSaldo
+	 */
 	public void depositar(Cuenta cuenta, int monto) throws ErrorAlIntroducirSaldo {
 		
 		cuenta.depositarSaldo(monto);
