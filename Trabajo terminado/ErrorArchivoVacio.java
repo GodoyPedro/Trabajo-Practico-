9a6a0a5572853ed97c843acc9f5e3fe08c4478f5
Alan Valdez <65427262+AlanEzequielValdez@users.noreply.github.com>
2020-06-01 16:17:40 -0300

    Update ErrorArchivoVacio.java

--- Trabajo terminado/ErrorArchivoVacio.java
+++ Trabajo terminado/ErrorArchivoVacio.java
@@ -1,6 +1,13 @@
 
 public class ErrorArchivoVacio extends Exception {
 	
+	/**
+	 * Pre: Esta excepcion se utiliza en: OperadorDeArchivos.
+	 * 
+	 * Post: Lanza una excepcion cuando el archivo no contiene informacion.
+	 * 
+	 */
+	
 	public ErrorArchivoVacio(String mensaje) {
 		
 		super(mensaje);
